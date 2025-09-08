4fb43be21f895c56410f02961ccab2eb1f5c3f80
michal.capo@gmail.com <mgt7=KkZ7.WtNM,VC~,5>
2025-09-08 20:35:07 +0200

    update: example

--- examples/pages/counter.ts
+++ examples/pages/counter.ts
@@ -1,53 +1,53 @@
 import ui from "../../ui";
 import { Context } from "../../ui.server";
 
-class Model {
-	constructor(public Count: number) {}
+// class Model {
+//     constructor(public Count: number) { }
+// }
+
+interface Model {
+    Count: number;
 }
 
-const target = ui.Target();
-
 function render(ctx: Context, counter: Model): string {
-	function decrement(ctx: Context): string {
-		ctx.Body(counter);
+    const target = ui.Target();
 
-		counter.Count--;
+    function decrement(ctx: Context): string {
+        ctx.Body(counter);
 
-		if (counter.Count < 0) counter.Count = 0;
+        counter.Count--;
 
-		return render(ctx, counter);
-	}
+        if (counter.Count < 0) counter.Count = 0;
 
-	function increment(ctx: Context): string {
-		ctx.Body(counter);
+        return render(ctx, counter);
+    }
 
-		counter.Count++;
+    function increment(ctx: Context): string {
+        ctx.Body(counter);
 
-		return render(ctx, counter);
-	}
+        counter.Count++;
 
-	return ui.div(
-		"flex gap-2 items-center bg-purple-500 rounded text-white p-px",
-		target,
-	)(
-		ui
-			.Button()
-			.Click(ctx.Call(decrement, counter).Replace(target))
-			.Class("rounded-l px-5")
-			.Render("-"),
+        return render(ctx, counter);
+    }
 
-		ui.div("text-2xl")(String(counter.Count)),
+    return ui.div("flex gap-2 items-center bg-purple-500 rounded text-white p-px", target)(
+        ui
+            .Button()
+            .Click(ctx.Call(decrement, counter).Replace(target))
+            .Class("rounded-l px-5")
+            .Render("-"),
 
-		ui
-			.Button()
-			.Click(ctx.Call(increment, counter).Replace(target))
-			.Class("rounded-r px-5")
-			.Render("+"),
-	);
+        ui.div("text-2xl")(String(counter.Count)),
+
+        ui
+            .Button()
+            .Click(ctx.Call(increment, counter).Replace(target))
+            .Class("rounded-r px-5")
+            .Render("+"),
+    );
 }
 
 export function Counter(ctx: Context, start: number = 3): string {
-	return ui.div("flex flex-row gap-4 border rounded p-4")(
-		render(ctx, new Model(start)),
-	);
+    return render(ctx, { Count: start })
 }
+
